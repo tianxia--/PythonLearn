d6bf4839b2472314095e364f4f7a3bb0e06de845
chenpengfei <pengfei.chen@matrx.team>
2020-12-25 13:46:17 +0800

    First commit

---
+++ Test1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+print ("Hello, Python!")
+if True:
+	print("test1")
+else:
+	print("test2")
+	
+
+days = ['Monday','Tuesday','Wednesday',
+'Thursday','Friday']
+
+print(days)
+
+word = 'word'
+sentence = "this is a ju zi"
+paragraph = """this is a duan luo, has more juzi"""
+#测试日志打印
+print(word)
+print(sentence)
+print(paragraph)
+
+
+input("按下 enter 键退出，其他任意键显示...\n")
